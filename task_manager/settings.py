c47d582a267067e5eb8b62d39fe2fc99b0bb9d4c
Metaller <metaller7440326@gmail.com>
2021-05-18 22:07:41 +0300

    added new options for herniaco

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -116,16 +116,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-LANGUAGE_CODE = 'en-en'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-if '/app' in os.environ['HOME']:
-    import django_heroku
-    django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
